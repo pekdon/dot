aea01eabe3d620e3fafe31500c09a16bec21329d
Claes Nästén <pekdon@gmail.com>
2022-03-04 20:45:33 +0100

    toggle_kbd_layout.sh: use {0,1} instead of ? in sed
    Not sure what is causing the ? match to fail on NetBSD, just switched to
    {0,1} for now.

--- bin/toggle_kbd_layout.sh
+++ bin/toggle_kbd_layout.sh
@@ -6,7 +6,7 @@ LAYOUTS="us se no"
 # output from xprop:
 # _XKB_RULES_NAMES(STRING) = "evdev", "pc105", "us", "", ""
 layout=`xprop -root _XKB_RULES_NAMES | sed 's/[,"]//g' | cut -d ' ' -f 5`
-next_layout=`echo $LAYOUTS | sed "s/.*$layout \?//" | cut -d ' ' -f 1`
+next_layout=`echo $LAYOUTS | sed "s/.*$layout \{0,1\}//" | cut -d ' ' -f 1`
 if test "x$next_layout" = "x"; then
 	next_layout=`echo $LAYOUTS | cut -d ' ' -f 1`
 fi
