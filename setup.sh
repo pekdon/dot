6096c3ed6ffa8d664cb860a968802ba997073bcd
Claes Nästén <pekdon@gmail.com>
2022-12-19 09:06:22 +0100

    Setup gtk-3.0 theme

--- setup.sh
+++ setup.sh
@@ -9,12 +9,31 @@ DOT_DIR="$(cd `dirname $0` && pwd)"
 FONT_MASTER="https://github.com/cormullion/juliamono/raw/master"
 FONT_NAME="JuliaMono-Regular.ttf"
 
+GTK_THEME_MASTER="https://github.com/thesquash"
+GTK_THEME="gtk-theme-raleigh"
 PEKWM_THEME="pwm-p"
+
+verify_required_tools()
+{
+    for i in curl git; do
+	which $i >/dev/null 2>&1
+	if test $? -ne 0; then
+	    echo "required tool $i not found, aborting"
+	    exit 1
+	fi
+    done
+}
 
 build()
 {
     echo "build tools"
     make -C "$DOT_DIR/src"
+}
+
+make_dirs()
+{
+    echo "create non version control directory structure"
+    mkdir -p ~/git ~/projects ~/tmp/download
 }
 
 link_files()
@@ -58,6 +77,16 @@ download_fonts()
     fi
 }
 
+download_gtk_themes()
+{
+    if ! test -e "$HOME/git/$GTK_THEME"; then
+	echo "downloading gtk theme $GTK_THEME..."
+	(cd $HOME/git ; git clone $GTK_THEME_MASTER/$GTK_THEME.git)
+    fi
+    mkdir -p $HOME/.local/share/themes
+    ln -sf $HOME/git/$GTK_THEME/themes/* $HOME/.local/share/themes
+}
+
 download_pekwm_themes()
 {
     if ! test -e "$HOME/.pekwm/themes/pwm-p"; then
@@ -81,9 +110,13 @@ reload_config()
     fi
 }
 
+verify_required_tools
+
 build
+make_dirs
 link_files
 init_dot_emacs
 download_fonts
+download_gtk_themes
 download_pekwm_themes
 reload_config
